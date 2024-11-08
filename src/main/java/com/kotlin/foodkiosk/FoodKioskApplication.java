674ef655251d276c31378389355b9923e34d0a70
gooddle <vptl6559@gmail.com>
2024-11-08 15:52:16 +0900

    :tada: start project

---
+++ src/main/java/com/kotlin/foodkiosk/FoodKioskApplication.java
@@ -0,0 +1,13 @@
+package com.kotlin.foodkiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodKioskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kotlin/foodkiosk/FoodkioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotlin.foodkiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodkioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
